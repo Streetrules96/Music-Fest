d8681f10bcc922d55ac3112bb899798d28e78a0e
Юрий Сергиевский <72981294+Streetrules96@users.noreply.github.com>
2022-08-05 18:18:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    // burger =====================================================
+
+    $(".burger").click(function(event) {
+        $(".burger, .nav").toggleClass("active");
+        $("body").toggleClass("lock");
+    });
+
+    $('.nav').click(function(event) {
+        $('.burger,.nav').removeClass('active');
+        $('body').removeClass('lock');
+    });
+    
+    // fixed header ===============================================
+    
+    var header = $("#header"),
+        scrollOffset = $(window).scrollTop();
+    
+    checkScroll(scrollOffset);
+    
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop();
+        
+        checkScroll(scrollOffset);
+        
+    });
+    
+    function checkScroll(scrollOffset) {
+        if( scrollOffset > 0 ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    };
+
+    // smooth scroll =================================================
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        var $this = $(this);
+        var blockId = $(this).data("scroll");
+        var blockOffset = $(blockId).offset().top -77; //-63 это высота header (если он фиксед)
+        
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+        
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);  // 500 - скорость анимации (0.5 сек)
+    });
+   
+});
